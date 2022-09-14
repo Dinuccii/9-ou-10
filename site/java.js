d3f16b77f1b675de59c357240563f7bac4a59037
Dinuccii <105287898+Dinuccii@users.noreply.github.com>
2022-09-14 12:37:15 -0300

    Add files via upload

---
+++ site/java.js
@@ -0,0 +1,71 @@
+</style> 
+    
+    <link rel="stylesheet" id="elementor-icons-fa-solid-css" href="https://www.gamegratistm.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/solid.min.css?ver=5.15.3" type="text/css" media="all"> 
+    
+    <link rel="stylesheet" id="elementor-icons-fa-regular-css" href="https://www.gamegratistm.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/regular.min.css?ver=5.15.3" type="text/css" media="all"> 
+    
+    <link rel="stylesheet" id="elementor-icons-fa-brands-css" href="https://www.gamegratistm.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/brands.min.css?ver=5.15.3" type="text/css" media="all"> 
+    
+    <script type="text/javascript" id="likebtn_frontend-js-extra"> 
+    
+    /* <![CDATA[ */ 
+    
+    var likebtn_eh_data = {"ajaxurl":"https:\/\/www.gamegratistm.com\/wp-admin\/admin-ajax.php","security":"a6c0c49160"}; 
+    
+    /* ]]> */ 
+    
+    </script> 
+    
+    <script type="text/javascript" src="https://www.gamegratistm.com/wp-content/plugins/likebtn-like-button/public/js/frontend.js?ver=6.0.2" id="likebtn_frontend-js"></script> 
+    
+    <script type="text/javascript" src="https://www.gamegratistm.com/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script> 
+    
+    <script type="text/javascript" src="https://www.gamegratistm.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script> 
+    
+    <script type="text/javascript" id="custom-js-extra"> 
+    
+    /* <![CDATA[ */ 
+    
+    var ajax_posts = {"ajaxurl":"https:\/\/www.gamegratistm.com\/wp-admin\/admin-ajax.php","noposts":"No older posts found"}; 
+    
+    /* ]]> */ 
+    
+    </script> 
+    
+    <script type="text/javascript" src="https://www.gamegratistm.com/wp-content/themes/buddyx-child/js/custom.js?ver=1.0" id="custom-js"></script> 
+    
+    <script type="text/javascript" id="buddyx-navigation-js-extra"> 
+    
+    /* <![CDATA[ */ 
+    
+    var buddyxScreenReaderText = {"expand":"Expand child menu","collapse":"Collapse child menu"}; 
+    
+    /* ]]> */ 
+    
+    </script> 
+    
+    <script type="text/javascript" src="https://www.gamegratistm.com/wp-content/themes/buddyx/assets/js/navigation.min.js?ver=2.9.2" id="buddyx-navigation-js" async=""></script> 
+    
+    <script type="text/javascript" defer="defer" src="https://www.gamegratistm.com/wp-content/themes/buddyx/assets/js/doubletap.min.js?ver=2.9.2" id="buddyx-doubletap-js" async=""></script> 
+    
+    <script type="text/javascript" src="https://www.gamegratistm.com/wp-content/themes/buddyx/assets/js/isotope.pkgd.min.js?ver=2.9.2" id="buddyx-isotope-pkgd-js" async=""></script> 
+    
+    <script type="text/javascript" defer="defer" src="https://www.gamegratistm.com/wp-content/themes/buddyx/assets/js/fitvids.min.js?ver=2.9.2" id="buddyx-fitvids-js" async=""></script> 
+    
+    <script type="text/javascript" src="https://www.gamegratistm.com/wp-content/themes/buddyx/assets/js/sticky-kit.min.js?ver=2.9.2" id="buddyx-sticky-kit-js" async=""></script> 
+    
+    <script type="text/javascript" src="https://www.gamegratistm.com/wp-content/themes/buddyx/assets/js/custom.min.js?ver=2.9.2" id="buddyx-custom-js" async=""></script> 
+    
+    <link rel="https://api.w.org/" href="https://www.gamegratistm.com/wp-json/"><link rel="alternate" type="application/json" href="https://www.gamegratistm.com/wp-json/wp/v2/pages/37123"><link rel="shortlink" href="https://www.gamegratistm.com/"> 
+    
+    <link rel="alternate" type="application/json+oembed" href="https://www.gamegratistm.com/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fwww.gamegratistm.com%2F"> 
+    
+    <link rel="alternate" type="text/xml+oembed" href="https://www.gamegratistm.com/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fwww.gamegratistm.com%2F&amp;format=xml"> 
+    
+    <script type="text/javascript">var ajaxurl = 'https://www.gamegratistm.com/wp-admin/admin-ajax.php';</script> 
+    
+    <style id="wplmi-inline-css" type="text/css"> 
+    
+    .post-modified-info{margin:5px 10px;font-size: 14px;color: #00000078;text-align: right;} span.wplmi-user-avatar { width: 16px;display: inline-block !important;flex-shrink: 0; } img.wplmi-elementor-avatar { border-radius: 100%;margin-right: 3px; } 
+    
+    </style> 
